7808c8be24aa5d574aba9212e3e9aa39d37a1aed
Ishat Gupta <62379502+LePichu@users.noreply.github.com>
2026-05-15 09:55:28 +0530

    feat: merge nav into header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,23 @@
-import Nav from "./Nav"
+function Nav() {
+	const NAV_URLS: Record<string, string> = {
+		"/": "/",
+		"/main": "https://lepichudoes.deno.dev/",
+		"/contact": "https://instagram.com/lepichudoesdev",
+	}
+
+	return (
+		<nav class="grid grid-cols-3 gap-4 font-mono w-full">
+			{Object.entries(NAV_URLS).map(([k, v]) => (
+				<a
+					href={v}
+					class="p-4 hover:bg-neutral-100 border border-neutral-300"
+				>
+					{k}
+				</a>
+			))}
+		</nav>
+	)
+}
 
 export default function Header() {
 	return (
