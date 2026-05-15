81838a6e1447f035b9ed1c9adf3156941ab9041a
Ishat Gupta <62379502+LePichu@users.noreply.github.com>
2026-05-15 09:27:49 +0530

    fix: header padding

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import Nav from "./Nav"
 
 export default function Header() {
 	return (
-		<header class="md:p-8 p-4 flex items-center justify-center w-full">
+		<header class="p-4 flex items-center justify-center w-full">
 			<div class="w-full md:w-3xl md:mx-auto">
 				<Nav />
 			</div>
